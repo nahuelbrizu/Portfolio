d551668b175c9b57018a66d1cde125e3e1c97ddf
Nawe <nahuelbrizu@outlook.com>
2022-04-22 19:41:38 -0300

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -8,7 +8,6 @@ function Type() {
         strings: [
           "< / > ",
           "Full Stack Developer",
-          "I have my interest set on continuing improving my skills, constantly updating  my knowledg in the  Programming field ",
           "I { ❤ } programming",
           "Thanks for your time",
           "< / >",
