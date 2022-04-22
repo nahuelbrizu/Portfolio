e606d1dc04adec15a6f9d2cd468b81f55d85c58a
Nawe <nahuelbrizu@outlook.com>
2022-04-22 18:14:22 -0300

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,11 +6,13 @@ function Type() {
     <Typewriter
       options={{
         strings: [
+          "< / > ",
           "Full Stack Developer",
-          "I'm interested in further learning to polish my skills",
           "I have my interest set on continuing improving my skills, constantly updating  my knowledg in the  Programming field ",
           "I { ❤ } programming",
           "Thanks for your time",
+          "< / >",
+          "...",
         ],
         autoStart: true,
         loop: true,
