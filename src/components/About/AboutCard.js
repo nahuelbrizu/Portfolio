c2c407e8aac545e80d1befd7dd75efe15354f06c
Nawe <nahuelbrizu@outlook.com>
2022-04-22 16:22:15 -0300

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,30 +8,32 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
-            <br />I am a junior pursuing IMSC in Maths and Computing in BIT
-            Mesra.
+            Hi Everyone, I am <span className="purple">Nahuel Brizuela </span>
+            from <span className="purple"> Buenos Aires, Argentina.</span>
+            <br />I am a self-taught programmer
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
           </p>
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games
+              <ImPointRight />   Study Code
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writting Tech Blogs
+              <ImPointRight />  Play Music
             </li>
             <li className="about-activity">
-              <ImPointRight /> Travelling
+              <ImPointRight />  Travelling
+            </li>
+            <li className="about-activity">
+              <ImPointRight />  Playing Games
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Nahuel Brizuela</footer>
         </blockquote>
       </Card.Body>
     </Card>
